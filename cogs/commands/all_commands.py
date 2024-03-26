bf744cde3ef7c29866bcdd96fd307ae31d2a3c6f
alukxrd <87390163+alukxrd@users.noreply.github.com>
2024-03-27 00:21:42 +0300

    Update all_commands.py

--- cogs/commands/all_commands.py
+++ cogs/commands/all_commands.py
@@ -10,11 +10,10 @@ class About(commands.Cog):
     async def anonim(self, ctx):
         pass
 
-
     @anonim.sub_command(name="report", description="отправьте анонимное сообщение о нарушении")
     async def report(self, ctx, message):
         #нарушители
-        channel_to_send = self.bot.get_channel(1012046671963750411)
+        channel_to_send = self.bot.get_channel(1012046671963750411)                
         channel_log = self.bot.get_channel(1012044889690738698)
         role_mod = get(ctx.guild.roles, name="Модератор")
         embed_message = Embed(title="Замечено нарушение",description=f"анонимное сообщение:\n\n{message}\n",color=0x000000) 
@@ -27,17 +26,16 @@ class About(commands.Cog):
                             f"В канале {ctx.channel.mention}\n"
                             f"Текст:\n{message}\n\n",  
                             color=0x000000)
-            embed.set_footer(text="Всё обсуждение данного нарушения должно происходить через личные сообщения для сохранения анонимности")
             await channel_log.send(embed=embed)
         except Exception as e:
             print(f"ERROR: echo, {e}")
-       
+
 
     @anonim.sub_command(name="about", description="информация о боте")
     async def about(self, ctx):
         channel_log = self.bot.get_channel(874524444457066557)
         channel_nar = self.bot.get_channel(1012046671963750411)
-        creator = self.bot.get_user(1006203420635582464)
+        creator     = self.bot.get_user(    1006203420635582464)
         try:
             embed = Embed(
                 title="**Обо мне:**",
@@ -46,20 +44,18 @@ class About(commands.Cog):
                 f"Но предупреждаю, если вы будете спамить, мой создатель все увидит и выдаст соответствующее наказание.\n"
                 f"Если вы хотите отправить жалобу на кого-либо, испольуйте команду `/echo`, расскажите о проблеме,"
                 f"позже модераторы рассмотрят вашу заявку\n\n"
-                f"Создан: {creator.mention}, специально для osu!#russian"
+                f"Создан: {creator.mention}, специально для discord сервера 'name'"
             )
-      
             await ctx.response.send_message(embed=embed, ephemeral=True)
 
             embed_log = Embed(title="**была использована команда `about`**", 
-                description=f"использовал {ctx.user.mention}\nв канале {ctx.channel.mention}",  color=0x000000)
+                              description=f"использовал {ctx.user.mention}\nв канале {ctx.channel.mention}",  
+                              color=0x000000)
             await channel_log.send(embed=embed_log)
 
         except Exception as e:
             print(f"ERROR: echo, {e}")
 
 
-
 def setup(bot: commands.Bot):
     bot.add_cog(About(bot))
-    print(f">>> Cogs  | is ready  | {__name__}")
