858bc8749ff6e9652a2860ea704935f3fe0cce3a
alukxrd <87390163+alukxrd@users.noreply.github.com>
2024-03-27 00:08:07 +0300

    Update loads.py

--- core/loads.py
+++ core/loads.py
@@ -4,22 +4,17 @@ from disnake.ext import commands
 
 
 def load_cogs(bot):
-
     for filename in listdir('./cogs/event'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.event.{filename[:-3]}')
-
     for filename in listdir('./cogs/commands'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.commands.{filename[:-3]}')
-
-
-    print(">>> TOKEN | is ready  |", "―――――――――――――――――――――――――――――――――――――――――",
-          ">>> BOT   | starting  |", sep="\n")
+    print('any print')
 
 
 def load_token():
-    token = "MTA2ODE0MDA3NjY3ODI1ODczOQ.GHmOF0.zG1vrX-dYCBzrHL9F37tQadEfRqabk_2-d-ads"
+    token = '' #my token from .ini
     return token
 
 
